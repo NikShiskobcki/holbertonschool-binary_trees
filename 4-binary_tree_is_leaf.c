da45faa05fa5072959f66e6e912e7ac6f64b4c1f
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-15 10:20:55 -0700

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - main
+ * @node: node to check
+ * Return: 1 if leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if ((node->right == NULL) && (node->left == NULL))
+		return (1);
+	else
+		return (0);
+}
