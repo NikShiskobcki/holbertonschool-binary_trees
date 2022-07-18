e1155b858d601e61f943cb872bbbbd2e347950c6
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-18 09:48:55 -0700

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,15 +1,17 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
+ * binary_tree_height - main
+ * @tree: tree
+ * Return: size_t
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t rightH = 0, leftH = 0;
 
 	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
 	rightH = binary_tree_height(tree->right) + 1;
