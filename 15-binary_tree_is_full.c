fa0ed53aaf39bfdf87709594bb905696a5e63d44
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-19 10:50:58 -0700

    task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - main
+ * @tree: tree
+ * Return: int
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		retun (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
