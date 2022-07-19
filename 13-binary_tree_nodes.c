206e639836d3be500043f26da64044f799dc3a3b
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-19 10:37:41 -0700

    task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - main
+ * @tree: tree
+ * Return: size_t
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t leftH, rightH;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	leftH = binary_tree_nodes(tree->left);
+	rightH = binary_tree_nodes(tree->right);
+
+	return (leftH + 1 + rightH);
+}
