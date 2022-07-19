686cc8d8fba60b7910f962bf2abd8bed7abb1d92
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-19 11:06:10 -0700

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -45,16 +45,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	binary_tree_t *left, *right;
+
 	if (tree == NULL)
 		return (0);
 	if (binary_tree_is_leaf(tree))
 		return (1);
-	if (tree->left == NULL || tree->right == NULL)
+
+	left = tree->left;
+	right = tree->right;
+
+	if (left == NULL || right == NULL)
 		return (0);
-	if (binary_tree_height(tree->right) == binary_tree_height(tree->left))
+	if (binary_tree_height(right) == binary_tree_height(left))
 	{
-		if (binary_tree_is_perfect(tree->right) && binary_tree_is_perfect(tree->left))
+		if (binary_tree_is_perfect(right) && binary_tree_is_perfect(left))
 			return (1);
-	}	
+	}
 	return (0);
 }
